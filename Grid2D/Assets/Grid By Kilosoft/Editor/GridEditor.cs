3c2e9c988149641f201761239719a98aa5256f0e
Kilosoft <kilosoft@mail.ru>
2016-09-26 22:53:06 +0300

    Первая загрузка

---
+++ Grid2D/Assets/Grid By Kilosoft/Editor/GridEditor.cs
@@ -0,0 +1,66 @@
+﻿///
+/// Name: Grid By Kilosoft
+/// Author: Kilosoft (Шаганов Артем)
+/// Version: 0.1
+/// Date: 26.09.2016
+/// 
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Grid))]
+public class GridEditor : Editor
+{
+
+    Grid grid;
+        
+    void OnEnable() { EditorApplication.update += Update; }
+
+    [MenuItem("Kilosoft/Grid")]
+    public static void Init()
+    {
+        new GameObject("Grid By Kilosoft", typeof(Grid));
+    }
+
+    public override void OnInspectorGUI()
+    {
+        grid = (Grid)target;
+
+        EditorGUILayout.LabelField ("Настройка шага:",EditorStyles.boldLabel);
+        grid.Width = EditorGUILayout.FloatField ("По оси X", grid.Width);
+        grid.Height = EditorGUILayout.FloatField ("По оси Y", grid.Height);
+        grid.Draw = EditorGUILayout.Toggle("Показывать", grid.Draw);
+
+        EditorGUILayout.LabelField("Настройка сетки:", EditorStyles.boldLabel);
+        grid.CountLines = EditorGUILayout.IntSlider("Кол-во линий", (int)grid.CountLines, 1, 1000);
+
+        EditorGUILayout.LabelField ("Выбранные объекты:",EditorStyles.boldLabel);
+        foreach (var obj in Selection.objects)
+        {
+            EditorGUILayout.TextField("Имя объекта:", obj.name);
+        }
+
+        grid.name = "Grid By Kilosoft";
+        EditorUtility.SetDirty(grid);
+
+        EditorGUILayout.LabelField("О программе:", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Name: Grid By Kilosoft\nAuthor: Kilosoft\nVersion: 0.1", EditorStyles.helpBox);
+    }
+
+    void Update()
+    {
+        grid = (Grid)target;
+
+        if (grid.Draw)
+        {
+            foreach (var obj in Selection.objects)
+            {
+                Vector3 pos = (obj as GameObject).transform.position;
+                float x = Mathf.CeilToInt(pos.x / grid.Width) * grid.Width;
+                float y = Mathf.CeilToInt(pos.y / grid.Height) * grid.Height;
+                float z = pos.z;
+                (obj as GameObject).transform.position = new Vector3(x, y, z);
+            }
+        }
+    }
+
+}
---
+++ Grid2D/Assets/Grid By Kilosoft/Grid.cs
@@ -0,0 +1,45 @@
+﻿///
+/// Name: Grid By Kilosoft
+/// Author: Kilosoft (Шаганов Артем)
+/// Version: 0.1
+/// Date: 26.09.2016
+/// 
+
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+	public float Width = 1f;
+	public float Height = 1f;
+	public bool Draw = true;
+
+    public float CountLines = 10f;
+
+    void OnDrawGizmos()
+    {
+		if (Width < 0.1f) Width = 0.1f;
+		if (Height < 0.1f) Height = 0.1f;
+        
+		if (Draw)
+        {
+            var xVar = (CountLines / 2f) * Width;
+            for (float x = -xVar; x < xVar; x += Width)
+            {
+                var c = Mathf.Floor(x / Width) * Width + Width / 2f;
+                var a = new Vector3(c, -CountLines * Width / 2f - Width / 2f);
+                var b = new Vector3(c, CountLines * Width / 2f - Width / 2f);
+                Gizmos.DrawLine(a, b);
+            }
+
+            var yVar = (CountLines / 2f) * Height;
+            for (float y = -yVar; y < yVar; y += Height)
+            {
+                var c = Mathf.Floor(y / Height) * Height + Height / 2f;
+                var a = new Vector3(-CountLines * Height / 2f - Height / 2f, c);
+                var b = new Vector3(CountLines * Height / 2f - Height / 2f, c);
+                Gizmos.DrawLine(a, b);
+            }
+		}
+
+    }
+}
